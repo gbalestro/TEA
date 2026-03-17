b99cdc7c52c6da1c2e9af73b13feb8fa9a4c0762
Guilherme Balestro <gbalestro@gmail.com>
2026-03-17 18:46:16 +0000

    Testing Frontend and git pull/push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import teaImage from './assets/logoTEA_PNG.png'
+import teaImageSmall from './assets/logoTEA_small.png'
 import './App.css'
 
 function App() {
@@ -11,44 +13,42 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+         <a href="https://github.com/gbalestro/TEA" target="_blank">
+          <img src={teaImage} className="base" width="170" height="179" alt="" />
+         </a>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+          <div>
+            <h1>TEA</h1>
+            <h2>Track Everything App</h2>  
+            <p>
+              by Gui Balestro, Gui Silveira & Cesar Garcia
+              <nav>
+                with Coffee & tea
+              </nav>
+            </p>
+          </div>
         <button
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Like ❤ {count}
         </button>
       </section>
 
       <div className="ticks"></div>
 
       <section id="next-steps">
-        <div id="docs">
+        <div className='centerDiv' id="docs">
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <p>Read about our project</p>
           <ul>
             <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
+              <a href="https://github.com/gbalestro/TEA/blob/main/README.md" target="_blank">
+                <img className="logo" src={teaImageSmall} alt="" />
+                Readme
               </a>
             </li>
           </ul>
@@ -58,10 +58,10 @@ function App() {
             <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
+          <p>Join the community</p>
           <ul>
             <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
+              <a href="https://github.com/gbalestro/" target="_blank">
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -69,43 +69,31 @@ function App() {
                 >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
-                GitHub
+                Gui Balestro
               </a>
             </li>
             <li>
-              <a href="https://chat.vite.dev/" target="_blank">
+              <a href="https://github.com/silveiraguilherme" target="_blank">
                 <svg
                   className="button-icon"
                   role="presentation"
                   aria-hidden="true"
                 >
-                  <use href="/icons.svg#discord-icon"></use>
+                  <use href="/icons.svg#github-icon"></use>
                 </svg>
-                Discord
+                Gui Silveira
               </a>
             </li>
             <li>
-              <a href="https://x.com/vite_js" target="_blank">
+              <a href="https://github.com/Cesargarciajr/" target="_blank">
                 <svg
                   className="button-icon"
                   role="presentation"
                   aria-hidden="true"
                 >
-                  <use href="/icons.svg#x-icon"></use>
+                  <use href="/icons.svg#github-icon"></use>
                 </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
+                Cesar Garcia
               </a>
             </li>
           </ul>
