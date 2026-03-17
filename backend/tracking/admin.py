1cb666d3bd40221d2d914220dbdf8aab88105ec1
Guilherme Balestro <gbalestro@gmail.com>
2026-03-17 15:26:45 +0000

    Frontend changed from TS to JS

--- backend/tracking/admin.py
+++ backend/tracking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Person, Location, TimeLog
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Location)
+admin.site.register(TimeLog)
--- backend/tracking/models.py
+++ backend/tracking/models.py
@@ -18,7 +18,7 @@ class TimeLog(models.Model):
     # Relacionamento com Person (Epic: Goal Management)
     who = models.ManyToManyField(Person, related_name='logs')
     where = models.ForeignKey(Location, on_delete=models.CASCADE)
-    clock_in = models.DateTimeField(auto_now_add=True)
+    clock_in = models.DateTimeField(auto_now_add=False)
     clock_out = models.DateTimeField(null=True, blank=True)
     
     # Para marcar como concluído ou importante
