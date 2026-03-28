89baff8276799e41a85bc62f4723cdc11b89cb27
Guilherme Balestro <gbalestro@gmail.com>
2026-03-28 00:52:17 +0000

    undo index.html + created .github/workflow/deploy.yml + vite.config.js altered

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TEA/', // Substitua pelo nome exato do seu repositório se for diferente
 })
+
